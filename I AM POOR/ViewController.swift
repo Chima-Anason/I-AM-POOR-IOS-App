fe0975152af38b6f9f20f32ff5d822024ce7808e
Chima-Anason <tblynxx@gmail.com>
2021-12-06 03:04:10 +0800

    Initial Commit

---
+++ I AM POOR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM POOR
+//
+//  Created by Anason Chima on 5/18/20.
+//  Copyright © 2020 Anason Chima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
